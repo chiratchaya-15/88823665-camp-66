8160019352792c8d32a47390db1dc45405738570
chiratchaya-15 <cvsew828@gmail.com>
2024-12-25 19:28:32 +0700

    mylaravel

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="style">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <head>
+    <body>
+        <div class="container mt-5">
+            <from method="get" actico="">
+                <label for=
+                
+
+    
+
+
+
+
+
+
