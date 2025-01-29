e583d85c61f4d48ecb8682fdee6c74189ed82c8c
chiratchaya-15 <cvsew828@gmail.com>
2025-01-29 14:55:25 +0700

    error

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\mycontroller;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/mycontroller/{id?}',
-[MyController::class, 'myfunction']);
+Route::get('/mycontroller/{id?}', [MyController::class, 'myfunction']);
 
 Route::post('/mycontroller/{id?}',
 [MyController::class, 'myfunction']);
@@ -18,3 +20,16 @@ function ($val="") {
     return "<h1>Hello Word $val<h1>";
 }
 );
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::get('/404', function() {
+    abort(404);
+});
+
+Route::get('/500', function() {
+    abort(500);
+});
